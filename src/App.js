cdad027a5f40783cddaaaed30a1dc95d20bd06c4
Aman Chaturvedi <alpha.amanc7@gmail.com>
2021-07-17 12:18:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Counter from "./Components/Counter";
 function App() {
   return (
     <div className="App">
-      // We can pass min and max props to the counter component, by default it will remain 1 and 1000 respectively
+      {/* We can pass min and max props to the counter component, by default it will remain 1 and 1000 respectively */}
       <Counter />
     </div>
   );
